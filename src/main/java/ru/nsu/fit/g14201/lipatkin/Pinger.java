7ec80bf2560939dd687758ccce950ebdb53b74c8
japson <lipatkin@bk.ru>
2017-09-09 17:55:48 +0700

    Add Pinger, Ponger, Parent classes

---
+++ src/main/java/ru/nsu/fit/g14201/lipatkin/Pinger.java
@@ -0,0 +1,10 @@
+package ru.nsu.fit.g14201.lipatkin;
+
+import se.sics.kompics.ComponentDefinition;
+
+/**
+ * Created by RARETA on 09.09.2017.
+ */
+public class Pinger extends ComponentDefinition {
+
+}
---
+++ src/main/java/ru/nsu/fit/g14201/lipatkin/Ponger.java
@@ -0,0 +1,9 @@
+package ru.nsu.fit.g14201.lipatkin;
+
+import se.sics.kompics.ComponentDefinition;
+
+/**
+ * Created by RARETA on 09.09.2017.
+ */
+public class Ponger extends ComponentDefinition {
+}
